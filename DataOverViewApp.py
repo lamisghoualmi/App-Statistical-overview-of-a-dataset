6f9e5735cf97be7e4381b34c1aa33d1816f4e790
lamis ghoualmi <56949028+lamisghoualmi@users.noreply.github.com>
2022-07-01 12:57:44 -0400

    Update DataOverViewApp.py

--- DataOverViewApp.py
+++ DataOverViewApp.py
@@ -14,11 +14,21 @@ import pandas  as pd
 st.header('Statistical description of a dataset')
 st.sidebar.image("AppStatOverView.png", use_column_width=True)
 st.sidebar.write("""
-        Making informed decisions requires a meticulous, step-by-step process of data analysis. 
-        Before beginning the analysis, it is crucial to take a look at the descriptive statistical overview 
-        of the dataset. This app helps you to get  a clear idea of the measures you need to  take to clean your data.
-For instance, spelling mistakes correction,  duplicates, missing values, variables with low variance, outliers, etc.
- If you are interested to check my code, check my github using 
+        Making informed decisions requires a meticulous, step-by-step process of data analysis. Before beginning the analysis,
+        it is crucial to look at the dataset's descriptive statistical overview. This app helps you get the general statistical 
+        description of a dataset, a clear idea of the measures you need to take to clean your data, and helps you ask the right 
+        questions. It has the following functionalities: First, the size of the data. Second, the dataset column names. Using this
+        might correct some misspelled columns' names or rename your variables to meaningful expressions that clearly express their purpose.
+        Third,  the data column types. Identifying the different data types helps you define which variables need to be formatted. 
+        Fourth, missing values are a common problem in data cleaning. Analyzing the percentage of missing values gives you a better 
+        notion of the method to use to process them. For example, removing missing values, applying an imputation technique
+        (replacing by zero, constant, mean, median, most frequent value, etc.). Fifth, the descriptive statistics summary of the 
+        numerical  data gives you an idea of the potential outliers that can have a disproportionate effect on your data. It also 
+        helps you get the variables with low variance. These kinds of variables add little to no information to your analysis and, 
+        therefore, are irrelevant and have to be removed. Sixth, descriptive statistics summary of the categorical data. This can be 
+        used to determine whether the population is biased or to possibly fix the category's error if any. For instance, if a user entered 
+        "SINGLE" and another one entered "ALONE," the information would fall under the same category, "SINGLE." So, this kind of error has
+        to be addressed before starting the analysis. If you are interested to check my code, check my github using 
         the following link: [Github](https://github.com/lamisghoualmi/-App-Statistical-overview-of-a-dataset).
          """)
 
